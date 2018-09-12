e537855bdef15f234b39a2f548633c681337a780
_dream <will.ferens@gmail.com>
2018-09-12 14:36:32 -0600

    added dependencies, fetched data in link list

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import LinkList from './LinkList'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <LinkList />
+      </div>
+    )
+  }
+}
+
+export default App
